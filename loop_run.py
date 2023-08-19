4aa0e3be097e9acbb85f689f2d480b8ffd0e4aae
LuciMoore <lucille.a.mo@gmail.com>
2023-08-18 20:37:31 -0500

    test on one subject

--- loop_run.py
+++ loop_run.py
@@ -1,10 +1,15 @@
 
 import step0_choose_templates
+import os
 
-
+aseg_dir='/home/feczk001/shared/projects/chir_corr/subject_data'
 age_months=12
 input_file='/home/feczk001/shared/projects/chir_corr/subject_data/sub-011228_ses-12mo.nii.gz'
+code_dir='/home/feczk001/shared/projects/nnUNet_s3_wrapper/chiralitycorrection/util'
 
 t1or2, template_anat, template_LR_mask = step0_choose_templates.choose_template(age_months)
 subject_file = step0_choose_templates.choose_subject_file(input_file, t1or2)
 print(subject_file, template_anat, template_LR_mask)
+subject_file=os.path.join('/', subject_file)
+
+os.system('sbatch {}/LR_mask_reg_sbatch.sh {} {} {}'.format(code_dir, subject_file, template_anat, template_LR_mask))
--- util/LR_mask_reg_sbatch.sh
+++ util/LR_mask_reg_sbatch.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
-set -e
+#!/bin/bash -l
 
-# This script generates a L/R mask for a subject. It first registers a template head (eg "1mo_T1w_acpc_dc_restore.nii.gz") to the subject's head. Next it applies
-# the resulting transformation matrix ("antsregAffine.txt") using Nearest Neighbor interpolation to register the template LR mask (eg 
-# "1mo_template_LRmask.nii.gz") to the subject head.
-
-# Important notes:
-# (1) Templates are age-specific, so make sure to select a template matching the age range of the subject. The 1mo template can be used for neonates.
-# (2) If available, it is recommended that one use T2w subject/template anatomicals for subjects 0-21 months of age and T1w subject/template anatomicals for 
-# subjects over 21 months old. The contrast in infant T2w scans makes the pial surface more visible and increases the quality of registration.
+#SBATCH -t 1:00:00
+#SBATCH -N 1
+#SBATCH --ntasks 4
+#SBATCH --mem=60gb
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lmoore@umn.edu
+#SBATCH -o /home/feczk001/shared/projects/nnUNet_s3_wrapper/chiralitycorrection/logs/%A_chir_corr.out
+#SBATCH -e /home/feczk001/shared/projects/nnUNet_s3_wrapper/chiralitycorrection/logs/%A_chir_corr.err
+#SBATCH -J chirality-correction
+#SBATCH -A feczk001
 
 SubjectHead=$1;shift
 TemplateHead=$1;shift
 TemplateMask=$1;shift
 
 module load ants
-WD="./wd"
+
+bn=$(basename "$SubjectHead")
+sub=`echo $bn | awk -F '_' '{print $1}'`
+ses=`echo $bn | awk -F '_' '{print $2}'`
+echo ${sub}_${ses}
+
+WD="./wd_${sub}_${ses}"
 mkdir "$WD"
 
 # Register the template head to the subject head
@@ -23,7 +30,7 @@ ANTS 3 -m CC["$SubjectHead","$TemplateHead",1,5] -t SyN[0.25] -r Gauss[3,0] -o "
 
 # Apply resulting transformation to template L/R mask to generate subject L/R mask
 antsApplyTransforms -d 3 \
-        --output LRmask.nii.gz \
+        --output ${sub}_${ses}_LRmask.nii.gz \
         --reference-image "$SubjectHead" \
         --transform "$WD"/antsregWarp.nii.gz "$WD"/antsregAffine.txt \
         --input "$TemplateMask" \
